ff675feb1ff1686dc547a7c80d7ab3539097581c
minisparrow <melchior_faith@163.com>
2024-09-23 22:08:44 +0800

    cycle list

--- linked-list/141-linked-list-cycle.cpp
+++ linked-list/141-linked-list-cycle.cpp
@@ -1,32 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <list>
-using namespace std;
-
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x): val(x), next(nullptr) {}
-};
-
+// case1: has loop
+// 3   -> 2(p)  -> 0  -> -4   -> 2(p)
+// 3sf -> 2(p)  -> 0  -> -4   -> 2(p)
+// 3   -> 2(p)s -> 0f -> -4   -> 2(p)
+// 3   -> 2(p)  -> 0s -> -4   -> 2(p)f
+// 3   -> 2(p)  -> 0  -> -4sf -> 2(p)
+//
+// case2: no loop
+// 3b -> 2  -> 0  -> -4
+// 3  -> 2s -> 0f -> -4
+// 3  -> 2  -> 0s -> -4 -> nullptr/f
+//
+// case2: no loop
+// 3b -> 2  -> 0  -> -4 -> 5
+// 3  -> 2s -> 0f -> -4 -> 5
+// 3  -> 2  -> 0s -> -4 -> 5f -> nullptr
+// b---------------------------------->
+// -sf-------------------------------->
+// --s-f------------------------------>
+// ---s--f---------------------------->
+// ----s---f-------------------------->
+#include "utils/listnode-common.h"
 class Solution {
 public:
   bool hasCycle(ListNode *head) {
     ListNode *slow = head;
     ListNode *fast = head;
-
     while (fast && fast->next) {
-       slow = slow->next;
-       fast = fast->next->next;
-       if(slow == fast) {
-         return true;
-       };
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+        return true;
+      }
     }
     return false;
   }
 };
-
-int main() {
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]) {
   ListNode *l1 = new ListNode(3);
   ListNode *l2 = new ListNode(2);
   ListNode *l3 = new ListNode(0);
@@ -35,14 +46,8 @@ int main() {
   l2->next = l3;
   l3->next = l4;
   l4->next = l2;
-
-  // while (l1) {
-  //   cout<< l1->val << endl;
-  // }
-
-  Solution *sln = new Solution();
-  bool result = sln->hasCycle(l1);
-  cout << result << endl;
-
+  // printList(l1);
+  Solution *sln = new Solution;
+  cout << sln->hasCycle(l1) << endl;
   return 0;
 }
