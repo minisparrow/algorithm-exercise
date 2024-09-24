cae971a423b4f544a72e209a12d2fedf75514df3
minisparrow <melchior_faith@163.com>
2024-09-24 20:52:27 +0800

    linked list cycle2

--- linked-list/142-linked-list-cycle2.cpp
+++ linked-list/142-linked-list-cycle2.cpp
@@ -1,39 +1,83 @@
-// https://leetcode.cn/problems/linked-list-cycle-ii/
-#include <iostream>
-#include <vector>
-#include <list>
-using namespace std;
-
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x): val(x), next(nullptr) {}
-};
-
+// case1: has loop
+// 3   -> 2(p)  -> 0  -> -4   -> 2(p)
+// 3sf -> 2(p)  -> 0  -> -4   -> 2(p)
+// 3   -> 2(p)s -> 0f -> -4   -> 2(p)
+// 3   -> 2(p)  -> 0s -> -4   -> 2(p)f
+// 3   -> 2(p)  -> 0  -> -4sf -> 2(p)
+//
+// case1: has loop
+// 3   -> 2(p)  -> 0  -> -4  -> 5   -> 2(p)
+// 3sf -> 2(p)  -> 0  -> -4  -> 5   -> 2(p)
+// 3   -> 2(p)s -> 0f -> -4  -> 5   -> 2(p)
+// 3   -> 2(p)  -> 0s -> -4  -> 5f  -> 2(p)
+// 3   -> 2(p)  -> 0f -> -4s -> 5   -> 2(p)
+// 3   -> 2(p)  -> 0  -> -4  -> 5sf -> 2(p)
+//
+// case1: has loop
+// 3   -> 2  -> 0  -> -4(p)   -> 5  -> -4(p)
+// 3sf -> 2  -> 0  -> -4(p)   -> 5  -> -4(p)
+// 3   -> 2s -> 0f -> -4(p)   -> 5  -> -4(p)
+// 3   -> 2  -> 0s -> -4(p)   -> 5f -> -4(p)
+// 3h  -> 2  -> 0  -> -4(p)s  -> 5  -> -4(p)f
+// 3   -> 2h -> 0  -> -4(p)   -> 5b -> -4(p)
+// 3   -> 2  -> 0h -> -4(p)   -> 5  -> -4(p)b
+// 3   -> 2  -> 0  -> -4(p)hb -> 5  -> -4(p)
+//
+//
+// s-------------o------x------------o
+// |------a------|--b---|-----c------|
+// o: 交叉点
+// x: 快慢指针相遇点
+// 相遇时快指针走的路程是慢指针的两倍：2(a + b) = a + b + c + b
+// 从公式得出： a = c,
+// 即从相遇点到交叉点的距离， 与起始点到交叉点距离相同
+//
+// case2: no loop
+// 3b -> 2  -> 0  -> -4
+// 3  -> 2s -> 0f -> -4
+// 3  -> 2  -> 0s -> -4 -> nullptr/f
+//
+// case2: no loop
+// 3b -> 2  -> 0  -> -4 -> 5
+// 3  -> 2s -> 0f -> -4 -> 5
+// 3  -> 2  -> 0s -> -4 -> 5f -> nullptr
+// b---------------------------------->
+// -sf-------------------------------->
+// --s-f------------------------------>
+// ---s--f---------------------------->
+// ----s---f-------------------------->
+//
+#include "utils/listnode-common.h"
 class Solution {
 public:
   ListNode *detectCycle(ListNode *head) {
     ListNode *slow = head;
     ListNode *fast = head;
-
+    // s-------------o------x------------o
+    // |------a------|--b---|-----c------|
+    // o: 交叉点
+    // x: 快慢指针相遇点
+    // 相遇时快指针走的路程是慢指针的两倍：2(a + b) = a + b + c + b
+    // 从公式得出： a = c,
+    // 即从相遇点到交叉点的距离， 与起始点到交叉点距离相同
     while (fast && fast->next) {
-       slow = slow->next;
-       fast = fast->next->next;
-       if(slow == fast) {
-         ListNode *slow2 = slow;
-         slow = head;
-         while (slow != slow2) {
-           slow = slow->next;
-           slow2 = slow2->next;
-         }
-         return slow2;
-       };
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+        ListNode *third = head;
+        while (third != slow) {
+          third = third->next;
+          slow = slow->next;
+        }
+        return third;
+      }
     }
     return nullptr;
   }
 };
-
-int main() {
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]) {
   ListNode *l1 = new ListNode(3);
   ListNode *l2 = new ListNode(2);
   ListNode *l3 = new ListNode(0);
@@ -42,14 +86,9 @@ int main() {
   l2->next = l3;
   l3->next = l4;
   l4->next = l2;
-
-  // while (l1) {
-  //   cout<< l1->val << endl;
-  // }
-
-  Solution *sln = new Solution();
-  ListNode *result = sln->detectCycle(l1);
-  cout << result->val << endl;
-
+  // printList(l1);
+  Solution *sln = new Solution;
+  cout << sln->detectCycle(l1)->val << endl;
+  printList(sln->detectCycle(l1));
   return 0;
 }
