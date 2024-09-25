3a9460a53ce983e0bf222307778899adbb8d4fdf
minisparrow <melchior_faith@163.com>
2024-09-26 07:39:18 +0800

    2 add two linked list

--- linked-list/2-add-two-numbers-linklist.cpp
+++ linked-list/2-add-two-numbers-linklist.cpp
@@ -1,61 +1,61 @@
-// low bit store at head of link list.
-#include <iostream>
-#include <vector>
-using namespace std;
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include "utils/listnode-common.h"
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+  // case 1----------
+  // 2 4 3
+  // 5 6 4
+  // +
+  // 7 0 8
+  // case 2----------
+  // 2 4 3
+  // 5 6
+  // +
+  // 7 0 4
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode *dummyNode = new ListNode(-1);
+    ListNode *addNode = dummyNode;
+    int valA;
+    int valB;
+    int carry = 0;
+    int acc;
+    while (l1 || l2) {
+      valA = (l1 != nullptr) ? l1->val : 0;
+      valB = (l2 != nullptr) ? l2->val : 0;
 
-      ListNode* dummyNode = new ListNode(-1);
-      ListNode* currNode = dummyNode;
-      int carry = 0;
-      for( ;l1 != nullptr || l2 != nullptr; currNode = currNode->next ) {
-        int c1 = l1 == nullptr ? 0 : l1->val;
-        int c2 = l2 == nullptr ? 0 : l2->val;
-        int val = (c1 + c2 + carry) % 10;
-        carry = (c1 + c2 + carry) / 10;
-        currNode->next = new ListNode(val);
+      acc = (valA + valB + carry) % 10;
+      carry = (valA + valB + carry) / 10;
 
-        l1 = l1 == nullptr ? nullptr : l1->next;
-        l2 = l2 == nullptr ? nullptr : l2->next;
-      }
-      if (carry > 0) currNode->next = new ListNode(carry);
-      return dummyNode->next;
+      addNode->next = new ListNode(acc);
+      addNode = addNode->next;
 
+      l1 = (l1 != nullptr) ? l1->next : nullptr;
+      l2 = (l2 != nullptr) ? l2->next : nullptr;
     }
+    if (carry)
+      addNode->next = new ListNode(carry);
+    return dummyNode->next;
+  }
 };
 
-
-int main() {
-  ListNode *l1 = new ListNode(2);
-  ListNode *l2 = new ListNode(4);
-  ListNode *l3 = new ListNode(3);
-  l1->next = l2;
-  l2->next = l3;
-
-  ListNode *p1 = new ListNode(5);
-  ListNode *p2 = new ListNode(6);
-  ListNode *p3 = new ListNode(4);
-
-  p1->next = p2;
-  p2->next = p3;
-
-  Solution* sln = new Solution();
-
-  ListNode* result = sln->addTwoNumbers(l1, p1);
-
-  while(result) {
-    cout << result->val << " ";
-    result = result->next;
-  }
-  cout << endl;
+int main(int argc, char *argv[]) {
+  vector<int> arr1{2, 4, 3};
+  vector<int> arr2{5, 6, 4};
+  ListNode *l1 = arrayToListNode(arr1);
+  ListNode *l2 = arrayToListNode(arr2);
+  printList(l1);
+  printList(l2);
+  Solution *sln = new Solution;
+  printList(sln->addTwoNumbers(l1, l2));
   return 0;
 }
